308f5763e04281f8ac234a1aa983c4dae0a5887a
Mike Lynch <micheal.j.lynch@gmail.com>
2026-01-05 17:06:09 -0500

    section 21.208 - set up uri components builder to eventually work with query params

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RequiredArgsConstructor
 @Service
@@ -21,7 +22,11 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<BeerDTOPageImpl> response = restTemplate.getForEntity(GET_BEERS_PATH, BeerDTOPageImpl.class);
+        // Spring will use the base URL from RestTemplate in conjuction with GET_BEERS_PATH
+        // components builder allows for us to utilize query params easier
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEERS_PATH);
+
+        ResponseEntity<BeerDTOPageImpl> response = restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
 
         return response.getBody();
     }
